d72cc9489b9a432e43ab9480497f9f38132aaa56
batmunkh0612 <cypox2017@gmail.com>
2023-12-27 11:01:13 +0800

    add put and delete query

--- backend/index.js
+++ backend/index.js
@@ -13,28 +13,25 @@ app.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`);
 });
 
-
-app.get("/user", async (req, res) => {
-    try {
-        const queryText =
-        `SELECT * FROM users`;
-        const response = await pool.query(queryText);
-        res.send(response.rows);
-      } catch (error) {
-        console.error(error);
-    }
+app.get("/users", async (_, res) => {
+  try {
+    const queryText = `SELECT * FROM users`;
+    const response = await pool.query(queryText);
+    res.send(response.rows);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-app.get("/users", async (req, res) => {
-    const { name, email } = req.body;
-    try {
-        const queryText =
-        `SELECT * FROM users WHERE name='${name}' AND email='${email}'`;
-        const response = await pool.query(queryText);
-        res.send(response.rows);
-      } catch (error) {
-        console.error(error);
-    }
+app.get("/user", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const queryText = `SELECT * FROM users WHERE name='${name}' AND email='${email}'`;
+    const response = await pool.query(queryText);
+    res.send(response.rows);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.post("/createTable", async (_, res) => {
@@ -43,7 +40,7 @@ app.post("/createTable", async (_, res) => {
     CREATE TABLE IF NOT EXISTS users (
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
-      email VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL
     )`;
     await pool.query(tableQueryText);
     res.send("ok");
@@ -54,19 +51,42 @@ app.post("/createTable", async (_, res) => {
 
 app.post("/user", async (req, response) => {
   const { name, email } = req.body;
-  console.log(name, email, 'req.body');
+
   try {
     const queryText =
       "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *";
-    const res = await pool.query(queryText, [ name, email ]);
-    response.send(res.rows[0])
+    const res = await pool.query(queryText, [name, email]);
+    response.send(res.rows[0]);
   } catch (error) {
     console.error(error);
-    response.send('error query')
+    response.send("error query");
   }
 });
 
-// `
-//     INSERT INTO users (name, email) VALUES ($1, $2)
-//     INSERT INTO tableName (fields) Values ()
-// `
+app.delete("/user", async (req, response) => {
+  const { name, email, id } = req.body;
+
+  try {
+    const queryText = `DELETE FROM users WHERE (name = '${name}' AND email = '${email}') OR id = '${id}'`;
+    await pool.query(queryText);
+    response.send("ok");
+  } catch (error) {
+    response.send("error").end();
+    console.error(error);
+  }
+});
+
+app.put("/user", async (req, response) => {
+  const { name, email, id } = req.body;
+
+  try {
+    const queryText = `UPDATE users SET name = '${name}', email = '${email}' WHERE id = '${id}'`;
+    await pool.query(queryText);
+    response.send("updated");
+  } catch (error) {
+    response.send("error").end();
+    console.error(error);
+  }
+});
+
+
